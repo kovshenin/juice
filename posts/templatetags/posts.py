3c3f5c1cf0ec9f4998c710efb4013933e928ce44
Konstantin Kovshenin <kovshenin@live.com>
2010-10-18 20:18:03 +0400

    post archives (template tags)

---
+++ posts/templatetags/posts.py
@@ -0,0 +1,34 @@
+from django import template
+from juice.posts.models import Post
+
+register = template.Library()
+
+@register.tag
+def posts_archive(parser, token):
+	tag_name, archive_type = token.split_contents()
+	return PostsArchiveNode(archive_type)
+	
+class PostsArchiveNode(template.Node):
+	def __init__(self, archive_type):
+		self.archive_type = archive_type
+	def render(self, context):
+		archive = []
+		archive_type = self.archive_type
+		
+		if archive_type == "daily":
+			dates = Post.objects.dates('published', 'day', 'DESC')
+			for date in dates:
+				count = Post.objects.filter(published__day=date.day, published__month=date.month, published__year=date.year).count()
+				archive.append("<li>%s (%s)</li>" % (date.strftime("%d %B %Y"), count))
+		elif archive_type == "monthly":
+			dates = Post.objects.dates('published', 'month', 'DESC')
+			for date in dates:
+				count = Post.objects.filter(published__month=date.month, published__year=date.year).count()
+				archive.append("<li>%s (%s)</li>" % (date.strftime("%B %Y"), count))
+		elif archive_type == "yearly":
+			dates = Post.objects.dates('published', 'year', 'DESC')
+			for date in dates:
+				count = Post.objects.filter(published__year=date.year).count()
+				archive.append("<li>%s (%s)</li>" % (date.strftime("%Y"), count))
+			
+		return "<ul>%s</ul>" % "".join(archive)
